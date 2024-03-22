9065f1240255e6cc437b69bd8766346c5a14268e
aquiles2001 <aquiles.lencina@mi.unc.edu.ar>
2024-03-22 02:03:31 -0300

    Add and link libraries

--- src/main.c
+++ src/main.c
@@ -5,5 +5,8 @@ int main(int argc, char *argv[])
 {
     printf("main says hello!\n");
     greetings();
+
+    printf("Static lib function returned: %d\n", static_lib_function("Hello, static lib!"));
+    printf("Shared lib function returned: %d\n", shared_lib_function("Hello, shared lib!"));
     return 0;
 }
